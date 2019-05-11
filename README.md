ded6de55fcd4161aaebd3b41fd0ec6acd70047b7
hoschi-it <hoschi-it@web.de>
2019-05-11 14:18:32 +0200

    use prefix

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 Child Wordpress Theme of Catch Responsive, adapted for "FeG Bad Vilbel"s needs
 
 ## Things, that were changed regarding the parent theme
-- Mobile menu: showing text not only hamburger
+- Mobile menu: 
+    - showing text, not only hamburger
+    - white background for readability
+- Footer: Use dark background-color instead of none
+
+
+## Added features
+
+### Shortcodes
+- ct_calendar: embedds the calendar of feg bad vilbel
+- ct_callist: embedds upcoming events of feg bad vilbel
--- functions.php
+++ functions.php
@@ -1,19 +1,21 @@
-<?php add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+<?php
 
-function theme_enqueue_styles() {
-	wp_enqueue_style( 'catch-responsive-css', get_template_directory_uri() . '/style.css' );
+function crc_enqueue_styles() {
+    wp_register_style('crc-css', get_template_directory_uri().'/style.css');    
+    wp_enqueue_style( 'crc-css' );
 }
 
 // Add Shortcode for embbeding churchtools calender
-function embbed_churchtools_calendar() {
+function crc_embbed_churchtools_calendar() {
     return '<iframe src="https://fegbv.church.tools/?q=churchcal&embedded=true&viewname=calView" width="100%" height="1000px"> </iframe>';
 }
 
-function embbed_churchtools_callist() {
+function crc_embbed_churchtools_callist() {
     return '<iframe src="https://fegbv.church.tools/?q=churchcal&embedded=true&viewname=eventView" width="100%" height="1000px"> </iframe>';
 }
 
-add_shortcode( 'ct_calendar', 'embbed_churchtools_calendar' );
-add_shortcode( 'ct_callist', 'embbed_churchtools_callist');
+add_shortcode( 'ct_calendar', 'crc_embbed_churchtools_calendar' );
+add_shortcode( 'ct_callist', 'crc_embbed_churchtools_callist');
+add_action('wp_enqueue_scripts', 'crc_enqueue_styles');
 
 ?>
